fca53a1626f60aa033dbb7b768d61714ed1cbcde
Okogun Melody <okogunmelody@gmail.com>
2024-01-29 15:35:02 +0100

    using the DateTime type and the now method to update the time

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -12,12 +12,23 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
 
   void getData() async{
-    Uri url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
-    // Uri uri = Uri.parse(url);
+   
+   // make the request
+   Uri url = Uri.parse('https://worldtimeapi.org/api/timezone/Africa/Lagos');
     Response response = await get(url);
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+    // print(data);
+
+    //get properties from data
+    String datetime = data['datetime'];
+    String offset =  data['utc_offset'].substring(1,3);
+    // print(datetime); 
+    // print(offset);
+
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
 
 
