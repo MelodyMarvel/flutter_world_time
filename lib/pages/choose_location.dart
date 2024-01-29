e779907f7cf632502c6bd2062daceda38fd0186d
Okogun Melody <okogunmelody@gmail.com>
2024-01-29 13:08:55 +0100

    testing asynchronous code with initState

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({super.key});
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+  void getData(){
+
+    //similate network request for a username
+    Future.delayed(Duration(seconds: 3), (){
+      print('yoshi');
+    });
+
+    print('statement');
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Text('choose location screen'),
+    );
+  }
+}
